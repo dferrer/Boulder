a4f528caef96c794ead26430f8b4e0be8d371075
Joseph Barrow <joseph.d.barrow@gmail.com>
2014-03-01 16:06:47 -0500

    More testing

--- index.js
+++ index.js
@@ -1,19 +1,36 @@
-var controller = require('lib/controller'),
-	config = require('config');
-
-var started = false;
-
 simply.text({
-	title: 'Boulder',
-	body: 'Hold Select to start your workout!',
+  title: 'Accel Demo',
+  body: 'Press up to stream or select to peek.',
 }, true);
-
-simply.on('longClick', function(e) {
-	if(!started) {
-		controller.startRecording();
-		started = true;
-	} else {
-		controller.stopRecording();
-		started = false;
-	}
+ 
+/**
+ * Use simply.accelConfig({ rate: 100, samples: 25 }) to configure
+ * the hertz and accel data per batch for the accelData events.
+ * See the API reference at simplyjs.io for more information.
+ */
+ 
+var onAccelData = function(e) {
+  simply.body('data: ' + JSON.stringify(e.accel));
+};
+ 
+// Press up to begin accelData streaming
+// Pressing up multiple times will register the handler more than once so be careful. 
+simply.on('singleClick', 'up', function(e) {
+  simply.on('accelData', onAccelData);
+});
+ 
+// Press down until all accelData handlers are removed and you can accelPeek again
+simply.on('singleClick', 'down', function(e) {
+  simply.off('accelData', onAccelData);
+});
+ 
+// Press select to accelPeek
+simply.on('singleClick', 'select', function(e) {
+  if (simply.accelConfig().subscribe) {
+    // accelData and accelPeek can't happen simultaneously
+    return;
+  }
+  simply.accelPeek(function(e) {
+    simply.body('peek: ' + JSON.stringify(e.accel));
+  });
 });
