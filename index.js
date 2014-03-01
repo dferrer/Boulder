189e59d756db685bf1588269d83efc0eb2ad84ac
Joseph Barrow <joseph.d.barrow@gmail.com>
2014-03-01 17:12:02 -0500

    More testing

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-// var controller = require('lib/controller'),
-var	config = require('config');
-
-// var started = false;
-
 simply.text({
   title: 'Accel Demo',
   body: 'Press up to stream or select to peek.',
@@ -14,26 +9,13 @@ simply.text({
  * See the API reference at simplyjs.io for more information.
  */
 
- // setInterval(function() {
- //  console.log("running.");
- //  simply.accelPeek(function(e) {
- //    console.log(JSON.stringify(e));
- //    simply.body('peek: ' + JSON.stringify(e.accel));
- //  });
- // }, 500);
-
- console.log("Hello, World!");
- // simply.accelConfig({ rate: 100, samples: 25 });
- 
 var onAccelData = function(e) {
-  console.log(e);
-  simply.body('data: ' + JSON.stringify(e.accels));
+  simply.body('data: ' + JSON.stringify(e.accel));
 };
  
 // Press up to begin accelData streaming
 // Pressing up multiple times will register the handler more than once so be careful. 
 simply.on('singleClick', 'up', function(e) {
-  console.log("Hit up button.");
   simply.on('accelData', onAccelData);
 });
  
@@ -50,22 +32,6 @@ simply.on('singleClick', 'select', function(e) {
     return;
   }
   simply.accelPeek(function(e) {
-    console.log("Peaking!")
-    simply.body('peek: ' + JSON.stringify(e));
+    simply.body('peek: ' + JSON.stringify(e.accel));
   });
 });
-
-// simply.text({
-// 	title: 'Boulder',
-// 	body: 'Hold Select to start your workout!',
-// }, true);
-
-// simply.on('longClick', function(e) {
-// 	if(!started) {
-// 		controller.startRecording();
-// 		started = true;
-// 	} else {
-// 		controller.stopRecording();
-// 		started = false;
-// 	}
-// });
