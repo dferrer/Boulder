42b1c764a793a98cfab8a02458e1d802a3e2b6fe
Dylan <dylan@ubuntu.(none)>
2014-03-01 16:17:24 -0500

    More testing

--- index.js
+++ index.js
@@ -1,27 +1,19 @@
 var controller = require('lib/controller'),
 	config = require('config');
 
-// var started = false;
+var started = false;
 
 simply.text({
 	title: 'Boulder',
 	body: 'Hold Select to start your workout!',
 }, true);
 
-simply.on('singleClick', 'up', function(e) {
-  controller.startRecording();
+simply.on('longClick', function(e) {
+	if(!started) {
+		controller.startRecording();
+		started = true;
+	} else {
+		controller.stopRecording();
+		started = false;
+	}
 });
-
-simply.on('singleClick', 'down', function(e) {
-  controller.stopRecording();
-});
-
-// simply.on('longClick', function(e) {
-// 	if(!started) {
-// 		controller.startRecording();
-// 		started = true;
-// 	} else {
-// 		controller.stopRecording();
-// 		started = false;
-// 	}
-// });
