36356102f6942d650f76e8fe34551511a11f32da
Joseph Barrow <joseph.d.barrow@gmail.com>
2014-03-01 15:04:08 -0500

    Button working check

---
+++ config.js
@@ -0,0 +1,5 @@
+var config = {
+	key: "Key"
+};
+
+module.exporst = config;
--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-simply.title('Hello World!');
+var controller = require('lib/controller'),
+	config = require('config');
+
+var started = false;
+
+simply.on('longClick', function(e) {
+	if(!started)
+		controller.start_recording();
+	else
+		controler.stop_recording();
+});
