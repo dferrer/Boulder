735b2b7f88887bec241d4ba87d26ce2a664aea46
Hunter Leath <h@hunterleath.com>
2014-03-02 04:43:56 -0500

    Testing JS Interface

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ loadView();
 
 // Global Variables
 var isTraining = false;
+var isTesting = false;
 var trainingSite = "http://152.23.18.198:5000";
 
 // Keep our accelConfig
@@ -22,6 +23,9 @@ var onAccelData = function(e) {
   if(isTraining) {
     ajax({ url: trainingSite, method: 'post', data: {"data": JSON.stringify(e.accels) } }, function(data){});
   }
+  if(isTesting) {
+    ajax({ url: trainingSite + "/test", method: 'post', data: {"data": JSON.stringify(e.accels) } }, function(data){});
+  }
 };
 
 // Check for accelData all the time
@@ -29,11 +33,30 @@ simply.on('accelData', onAccelData);
 
 // Training Mode is turned on and off with Long Down Press
 simply.on('singleClick', 'down', function(e) {
+  if(isTesting) {
+    return;
+  }
   isTraining = !isTraining;
   if(isTraining) {
     simply.vibe("short");
     ajax({ url: trainingSite, method: 'post', data: {"data": '{"begin_rep": 1 }'} }, function(data){});
     simply.text({title: "Boulder Trainer", body: "Data is being logged to " + trainingSite}, true);
+  } else {
+    simply.vibe("short");
+    loadView();
+  }
+});
+
+// Training Mode is turned on and off with Long Down Press
+simply.on('singleClick', 'up', function(e) {
+  if(isTraining) {
+    return;
+  }
+  isTesting = !isTesting;
+  if(isTesting) {
+    simply.vibe("short");
+    ajax({ url: trainingSite, method: 'post', data: {"data": '{"begin_rep": 1 }'} }, function(data){});
+    simply.text({title: "Boulder Tester", body: "Data is being logged to " + trainingSite}, true);
   } else {
     simply.vibe("short");
     loadView();
