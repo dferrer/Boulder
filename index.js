46049ffe851d1f68e117c850b80364a5a92e1ee5
Dylan <dylan@ubuntu.(none)>
2014-03-01 20:14:39 -0500

    More testing

--- index.js
+++ index.js
@@ -2,22 +2,16 @@ simply.text({
   title: 'Accel Demo',
   body: 'Press up to stream or select to peek.',
 }, true);
- 
-/**
- * Use simply.accelConfig({ rate: 100, samples: 25 }) to configure
- * the hertz and accel data per batch for the accelData events.
- * See the API reference at simplyjs.io for more information.
- */
 
 var onAccelData = function(e) {
-  simply.body('data: ' + JSON.stringify(e.accel));
+  simply.body('data: ' + JSON.stringify(e.accel), true);
 };
  
 // Press up to begin accelData streaming
 // Pressing up multiple times will register the handler more than once so be careful. 
-simply.on('singleClick', 'up', function(e) {
+// simply.on('singleClick', 'up', function(e) {
   simply.on('accelData', onAccelData);
-});
+// });
  
 // Press down until all accelData handlers are removed and you can accelPeek again
 simply.on('singleClick', 'down', function(e) {
@@ -42,6 +36,7 @@ simply.on('singleClick', 'select', function(e) {
        console.log(JSON.stringify(e.accel));
       });
     }, 100);
+    
     // console.log(accels);
   } else {
     started = false;
