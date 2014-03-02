166e1977ef81b794ce0a9f90bf14e847787e515e
Hunter Leath <h@hunterleath.com>
2014-03-02 02:12:11 -0500

    Added Vibe

--- index.js
+++ index.js
@@ -6,33 +6,31 @@ function reset() {
 }
 reset();
 
+// Global Variables
 var isTraining = false;
 var trainingSite = "http://152.23.18.198:5000";
 
+// Keep our accelConfig
 simply.accelConfig({ rate: 50, samples: 5 });
-ajax({ url: trainingSite, method: 'post', data: {"data": '{"begin_rep": 1 }'} }, function(data){});
 
+// Checking for Accel Data
 var onAccelData = function(e) {
   if(isTraining) {
-    simply.body('data: ' + JSON.stringify(e.accel), true);
     ajax({ url: trainingSite, method: 'post', data: {"data": JSON.stringify(e.accels) } }, function(data){});
   }
 };
 
+// Check for accelData all the time
 simply.on('accelData', onAccelData);
 
-// Press down until all accelData handlers are removed and you can accelPeek again
-simply.on('singleClick', 'down', function(e) {
-  simply.off('accelData', onAccelData);
-});
-
+// Training Mode is turned on and off with Long Down Press
 simply.on('longClick', 'down', function(e) {
   isTraining = !isTraining;
   if(isTraining) {
-    simply.text({title: "Boulder Trainer", body: "Data is being logged to " + trainingSite})
+    simply.vibe("short");
+    ajax({ url: trainingSite, method: 'post', data: {"data": '{"begin_rep": 1 }'} }, function(data){});
+    simply.text({title: "Boulder Trainer", body: "Data is being logged to " + trainingSite});
   } else {
     reset();
   }
 });
-
-simply.on('')
