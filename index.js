45fd5f7482da8d1765287809f4c162b7e1185e43
Hunter Leath <h@hunterleath.com>
2014-03-02 02:06:48 -0500

    Changed Index

--- index.js
+++ index.js
@@ -1,55 +1,38 @@
-simply.text({
-  title: 'Accel Demo',
-  body: 'Press up to stream or select to peek.',
-}, true);
+function reset() {
+  simply.text({
+    title: 'Boulder',
+    body: 'Welcome to Boulder.',
+  }, true);
+}
+reset();
+
+var isTraining = false;
+var trainingSite = "http://152.23.18.198:5000";
 
 simply.accelConfig({ rate: 50, samples: 5 });
-ajax({ url: 'http://152.23.18.198:5000', method: 'post', data: {"data": '{"begin_rep": 1 }'} }, function(data){});
+ajax({ url: trainingSite, method: 'post', data: {"data": '{"begin_rep": 1 }'} }, function(data){});
 
 var onAccelData = function(e) {
-  simply.body('data: ' + JSON.stringify(e.accel), true);
-  // e.accels.forEach(function(samp) {
-  //   console.log(JSON.stringify(samp));
-  // });
-  ajax({ url: 'http://152.23.18.198:5000', method: 'post', data: {"data": JSON.stringify(e.accels) } }, function(data){});
-  // console.log('\n');
-  // console.log(JSON.stringify(e.accel))
+  if(isTraining) {
+    simply.body('data: ' + JSON.stringify(e.accel), true);
+    ajax({ url: trainingSite, method: 'post', data: {"data": JSON.stringify(e.accels) } }, function(data){});
+  }
 };
- 
-// Press up to begin accelData streaming
-// Pressing up multiple times will register the handler more than once so be careful. 
-// simply.on('singleClick', 'select', function(e) {
+
 simply.on('accelData', onAccelData);
-// });
- 
+
 // Press down until all accelData handlers are removed and you can accelPeek again
 simply.on('singleClick', 'down', function(e) {
   simply.off('accelData', onAccelData);
 });
- 
-// var started = false;
 
-// // Press select to accelPeek
-// simply.on('singleClick', 'select', function(e) {
-//   if (simply.accelConfig().subscribe) {
-//     console.log("Already Subscribed!")
-//     // accelData and accelPeek can't happen simultaneously
-//     return;
-//   }
-//   if (!started) {
-//     started = true;
-//     // accels = [];
-//     var intvl = setInterval(function() {
-//       simply.accelPeek(function(e) {
-//        // accels.push(e.accel);
-//        console.log(JSON.stringify(e.accel));
-//       });
-//     }, 100);
-    
-//     // console.log(accels);
-//   } else {
-//     started = false;
-//     // accels = [];
-//     clearInterval(intvl);
-//   }
-// });
+simply.on('longClick', 'down', function(e) {
+  isTraining = !isTraining;
+  if(isTraining) {
+    simply.text({title: "Boulder Trainer", body: "Data is being logged to " + trainingSite})
+  } else {
+    reset();
+  }
+});
+
+simply.on('')
