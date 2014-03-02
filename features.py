9347c83ed59b2be59ff78586566ab5f782ff0f35
Dylan <dylan@ubuntu.(none)>
2014-03-02 01:43:47 -0500

    Updated features

--- features.py
+++ features.py
@@ -1,8 +1,30 @@
-with open('data.csv', 'rU') as csvfile:
-	reader = csv.reader(csvfile)
-	for line in reader:
-		x = line[0]
-		y = line[1]
-		z = line[2]
-		time = line[3]
-		repnumber = line[4]
+import math
+import scipy as sp
+
+def get_features(lines):
+	current_rep = 1
+	x_sum = 0
+	y_sum = 0
+	z_sum = 0
+	x_array = []
+	y_array = []
+	z_array = []
+	time_array = []
+	for line in lines:
+		x_sum += line[0]
+		y_sum += line[1]
+		z_sum += line[2]
+		time_array.append(line[3])
+		if current_rep != line[4]:
+			current_rep = line[4]
+			RMS = math.sqrt(x_sum * x_sum + y_sum * y_sum + z_sum * z_sum)
+			x_integral = sp.integrate.simps(x_array, time_array)
+			y_integral = sp.integrate.simps(y_array, time_array)
+			z_integral = sp.integrate.simps(z_array, time_array)
+			x_sum = 0
+			y_sum = 0
+			z_sum = 0
+			x_array = []
+			y_array = []
+			z_array = []
+			time_array = []
