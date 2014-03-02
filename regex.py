777d315a892b23711075919d818888c88db8c809
Dylan <dylan@ubuntu.(none)>
2014-03-01 21:31:54 -0500

    More testing

--- regex.py
+++ regex.py
@@ -3,7 +3,20 @@ import re
 regexp = re.compile('^index\.js: \{\"x\":(.*),\"y\":(.*),\"z\":(.*),\"vibe\".*\"time\":(.*)\}0$')
 with open('data/barbell.training_data') as f:
 	contents = f.read().splitlines()[25:-50]
+	xs = []
+	ys = []
+	zs = []
 	for line in contents:
 		if line.strip() != 'index.js:  JS: Simply.js:' and line.strip() != '' and line[0] != '[':
 			m = re.search(regexp, line)
-			print m.group(1), m.group(2), m.group(3), m.group(4)
+			xs.append(m.group(1) + '\t' + m.group(4))
+			ys.append(m.group(2) + '\t' + m.group(4))
+			zs.append(m.group(3) + '\t' + m.group(4))
+	for x in xs:
+		print x
+	print ''
+	for y in ys:
+		print y
+	print ''
+	for z in zs:
+		print z
